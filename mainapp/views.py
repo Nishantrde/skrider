050b3a813fca5bf69383002136b7cf738df0619a
Nishant <nishant2024rde@gmail.com>
2024-05-27 00:17:10 -0700

    Initial Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ def index(request):
     return render(request, "index.html")
 
 def galery(request):
-    return render(request, "galery.html")
+    if request.method == "POST":
+        return render(request, "galery.html")
 
 
