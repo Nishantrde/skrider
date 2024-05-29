c6be89f9e3771f873638331c7d8bcf79c98802d0
Nishant <nishant2024rde@gmail.com>
2024-05-28 19:18:25 -0700

    Initial Commit

--- staticfiles_build/static/js/index.js
+++ staticfiles_build/static/js/index.js
@@ -17,4 +17,13 @@ window.addEventListener('scroll', function() {
       }
     });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const aboutSection = document.querySelector('#about');
+    const destinationsSection = document.querySelector('#destinations');
   
+    // Add fade-in class to about and destinations sections after a delay
+    setTimeout(function() {
+      aboutSection.classList.add('fade-in');
+      destinationsSection.classList.add('fade-in');
+    }, 500); // Adjust delay as needed
+  });
